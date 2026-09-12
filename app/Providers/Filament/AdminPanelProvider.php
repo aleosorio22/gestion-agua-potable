a2cd2457b08cecc05a0be501b8a17a246be07bdb
ded5t <ded5times@gmail.com>
2026-09-11 18:21:10 -0600

    feat: agregar reportes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,7 @@ class AdminPanelProvider extends PanelProvider
                 GrupoNavegacion::Padron->getLabel(),
                 GrupoNavegacion::Operacion->getLabel(),
                 GrupoNavegacion::Catalogos->getLabel(),
+                GrupoNavegacion::Reportes->getLabel()
             ])
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\Filament\Admin\Pages')
             ->pages([
